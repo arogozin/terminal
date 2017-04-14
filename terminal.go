18df2501420858eb6a4c1cf4c178560134da6474
Aleksandr Rogozin <aleksandr@rogozin.us>
2017-04-14 08:22:20 -0400

    clear and reset line

--- terminal.go
+++ terminal.go
@@ -2,10 +2,19 @@ package terminal
 
 import (
     "fmt"
+    "bytes"
 )
 
-const RESET = "\033[2J"
+const CLEAR = "\033[2J"
+
+var Screen *bytes.Buffer = new(bytes.Buffer)
 
 func Clear() {
-    fmt.Println(RESET)
+    fmt.Println(CLEAR)
+    MoveCursor(0, 0)
 }
+
+func MoveCursor(x int, y int) {
+    fmt.Printf("\033[%d;%dH", x, y)
+}
+

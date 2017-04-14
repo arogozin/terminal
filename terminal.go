485b19d49860efbd657dcd2e9b4d029fb53ae3f9
Aleksandr Rogozin <aleksandr@rogozin.us>
2017-04-14 08:08:04 -0400

    clearing up code

--- terminal.go
+++ terminal.go
@@ -1,33 +1,11 @@
 package terminal
 
 import (
-    "os"
-    "bufio"
-    "bytes"
-    "strings"
     "fmt"
 )
 
 const RESET = "\033[2J"
 
-var Output *bufio.Writer = bufio.NewWriter(os.Stdout)
-var Screen *bytes.Buffer = new(bytes.Buffer)
-
-
 func Clear() {
-    Output.WriteString(RESET)
-}
-
-func Flush() {
-    for i, str := range strings.Split(Screen.String(), "\n") {
-        if i > int(Height()) {
-            return
-        }
-        
-        fmt.Println(i)
-        fmt.Println(str)
-    }
-    
-    Output.Flush()
-    Screen.Reset()
+    fmt.Println(RESET)
 }

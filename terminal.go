4ad783933b59fbfd7a52328df7b709c985899f1c
Aleksandr Rogozin <aleksandr@rogozin.us>
2017-04-13 20:16:56 -0400

    updates

--- terminal.go
+++ terminal.go
@@ -4,6 +4,7 @@ import (
     "os"
     "bufio"
     "bytes"
+    "strings"
 )
 
 const RESET = "\033[2J"
@@ -14,4 +15,17 @@ var Screen *bytes.Buffer = new(bytes.Buffer)
 
 func Clear() {
     Output.WriteString(RESET)
+}
+
+func Flush() {
+    for i, str := range strings.Split(Screen.String(), "\n") {
+        if i > Height() {
+            return
+        }
+        
+        Output.WriteString(str + "\n")
+    }
+    
+    Output.Flush()
+    Screen.Reset()
 }

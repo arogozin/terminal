2819b54335e5f6b722b3ac7332beee57cbd7b32c
Aleksandr Rogozin <aleksandr@rogozin.us>
2017-04-15 22:00:58 -0400

    updates

--- terminal.go
+++ terminal.go
@@ -2,16 +2,33 @@ package terminal
 
 import (
     "fmt"
+    "bufio"
+    "os"
+    "bytes"
 )
 
 const CLEAR = "\033[H\033[2J\033[3J"
+const RESET = "\033[0m"
+
+const BOLD = "\033[1m%s\033[0m"
+
+var Output *bufio.Writer = bufio.NewWriter(os.Stdout)
+var Screen *bytes.Buffer = new(bytes.Buffer)
 
 func Clear() {
     fmt.Println(CLEAR)
-    MoveCursor(0, 0)
+    MoveCursor(0,0)
 }
 
 func MoveCursor(x int, y int) {
     fmt.Printf("\033[%d;%dH", x, y)
 }
 
+func Print(str string) {
+    fmt.Println(Output, str)
+    Output.Flush()
+}
+
+func Bold(str string) {
+    fmt.Printf(BOLD, str+"\n")
+}
